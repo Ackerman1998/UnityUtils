4f6d47d575c2c703314295c04054188e107f4c1e
Ackerman1998 <1019226202@qq.com>
2022-01-11 17:47:02 +0800

    add guidance module

---
+++ Assets/UnityGameUtils/Guidance/GuidanceEventPenetrate.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// 事件渗透
+/// </summary>
+public class GuidanceEventPenetrate : MonoBehaviour, ICanvasRaycastFilter
+{
+    private RectTransform targetImage;
+    public void SetTargetImage(RectTransform target)
+    {
+        targetImage = target;
+    }
+    public bool IsRaycastLocationValid(Vector2 sp, Camera eventCamera)
+    {
+        if (targetImage == null)
+        {
+            return true;
+        }
+       // return !RectTransformUtility.RectangleContainsScreenPoint(targetImage, sp, eventCamera);
+        return true;
+    }
+}
---
+++ Assets/UnityGameUtils/Guidance/TestGuide.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestGuide : MonoBehaviour
+{
+    public RectTransform target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        RectGuidance.instance.Init(target);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
